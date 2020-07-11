e4a9790b8949448cda2aff55bd4898964f03e7ca
taufikhidayat <mtaufiq.hidayat03@gmail.com>
2020-07-11 15:58:45 +0700

    first commit

---
+++ src/main/java/com/tutorial/springboot/restapi/model/User.java
@@ -0,0 +1,34 @@
+package com.tutorial.springboot.restapi.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_address", nullable = false)
+    private String email;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = false)
+    private Date createdAt;
+
+
+}
