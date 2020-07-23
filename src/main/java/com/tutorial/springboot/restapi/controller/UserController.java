da30973d036bd86d4ccb9214d4a599441c5ddd94
taufikhidayat <mtaufiq.hidayat03@gmail.com>
2020-07-23 14:28:19 +0700

    Rest Api Documentation

--- src/main/java/com/tutorial/springboot/restapi/controller/UserController.java
+++ src/main/java/com/tutorial/springboot/restapi/controller/UserController.java
@@ -3,6 +3,10 @@ package com.tutorial.springboot.restapi.controller;
 import com.tutorial.springboot.restapi.exception.ResourceNotFoundException;
 import com.tutorial.springboot.restapi.model.User;
 import com.tutorial.springboot.restapi.repository.UserRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +20,26 @@ import java.util.concurrent.RejectedExecutionException;
 
 @RestController
 @RequestMapping("/api/v1")
+@Api(value = "userController", description = "Operations of user controller")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @ApiOperation(value = "View a list of available users", response = User.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    @ApiOperation(value = "Search user by id", response = User.class)
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId)
             throws ResourceNotFoundException {
@@ -35,12 +49,14 @@ public class UserController {
         return ResponseEntity.ok().body(user);
     }
 
+    @ApiOperation(value = "Add new user")
     @PostMapping("/users")
     public User createUser(@Valid @ModelAttribute User user) {
-        System.out.println(user);
+        //System.out.println(user);
         return userRepository.save(user);
     }
 
+    @ApiOperation(value = "Update existing user")
     @PutMapping("/users/{id}")
     public ResponseEntity<User> updateUser(
             @PathVariable(value = "id") Long userId, @Valid @ModelAttribute User userData) throws ResourceNotFoundException {
@@ -56,6 +72,7 @@ public class UserController {
         return ResponseEntity.ok(updatedUser);
     }
 
+    @ApiOperation(value = "Delete existing user")
     @DeleteMapping("/users/{id}")
     public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws Exception {
         User user =
